953242cf410d2883889923414b6fe0afa8d7a28d
ColomboStefania <stefania.colombo3@gmail.com>
2018-07-12 21:53:07 +0200

    falling mario

--- src/components/games/GameDetails.js
+++ src/components/games/GameDetails.js
@@ -63,28 +63,45 @@ class GameDetails extends PureComponent {
      
 
       <div className = 'mario'>
+
+          { game.score !== null &&
+           game.score.filter(item => item === "o").length <= 10 &&
           <img src="https://i.pinimg.com/736x/2b/6b/cf/2b6bcf45c8a270d3563dc596bd6ba926--mario-birthday-party-birthday-games.jpg"  width="100" height="100" className = "pic"/>
-          { game.score !== null && game.score.filter(item => item === "x").length >= 12 &&
+          }
+
+          { game.score !== null && game.score.filter(item => item === "x").length >= 10 &&
           <img src="https://png.pngtree.com/element_origin_min_pic/17/07/23/473f204a1589862d0264b14f926b4b59.jpg"  width="100" height="100" className = "trophy" />
           }
           { game.score !== null &&
             game.score.filter(item => item === "x").length !== null &&
           <p className='scoreText'>Mario score: {game.score.filter(item => item === "x").length} points </p>
           } 
-      </div>
+
+           { game.score !== null && game.score.filter(item => item === "o").length >= 10 &&
+          <img src="https://i.pinimg.com/736x/2b/6b/cf/2b6bcf45c8a270d3563dc596bd6ba926--mario-birthday-party-birthday-games.jpg"  width="100" height="100" className = "fallingpic" />
+          }
+          </div>
 
 
        <div className = "ScoreBoard">
-      <img src="http://cdn8.bigcommerce.com/s-balh3740/images/stencil/608x608/products/9370/2621/luigi__96034__23458.1354626704.jpg?c=2"  width="100" height="100"  className = "pic"/>
+
+          { game.score !== null &&
+         game.score.filter(item => item === "x").length <= 10 &&
+         <img src="http://cdn8.bigcommerce.com/s-balh3740/images/stencil/608x608/products/9370/2621/luigi__96034__23458.1354626704.jpg?c=2"  width="100" height="100"  className = "pic"/>
+        } 
      
       { game.score !== null &&
          game.score.filter(item => item === "o").length !== null &&
       <p className='scoreText'>Luigi score: {game.score.filter(item => item === "o").length} points </p>
       } 
 
-       { game.score !== null && game.score.filter(item => item === "o").length >= 12 &&
+       { game.score !== null && game.score.filter(item => item === "o").length >= 10 &&
       <img src="https://png.pngtree.com/element_origin_min_pic/17/07/23/473f204a1589862d0264b14f926b4b59.jpg"  width="100" height="100"  />
       }
+
+       { game.score !== null && game.score.filter(item => item === "x").length >= 10 &&
+          <img src="http://cdn8.bigcommerce.com/s-balh3740/images/stencil/608x608/products/9370/2621/luigi__96034__23458.1354626704.jpg?c=2"  width="100" height="100" className = "fallingpic" />
+          }
 
       {/* {
          game.score.filter(item => item === "x").length &&
