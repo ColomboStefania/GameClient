ecf52d65bd4859ded388332651601aa383c950ad
ColomboStefania <stefania.colombo3@gmail.com>
2018-07-12 19:46:57 +0200

    more rules

--- src/components/games/Rules.js
+++ src/components/games/Rules.js
@@ -9,13 +9,19 @@ export default class Rules extends PureComponent {
 
         <Paper className="outer-paper">
         <div >
-        <h1 > Rules of the Game</h1>
-        <div className = "rulecontainer">
+        <h1 > Rules of the Game</h1><img src="https://vignette.wikia.nocookie.net/mario/images/8/8c/Mushroom.png/revision/latest?cb=20101223220717"  width="100" height="100" className ="rulesImage"/>
         <img src="https://vignette.wikia.nocookie.net/mario/images/8/8c/Mushroom.png/revision/latest?cb=20101223220717"  width="100" height="100" className ="rulesImage"/>
+        <img src="https://vignette.wikia.nocookie.net/mario/images/8/8c/Mushroom.png/revision/latest?cb=20101223220717"  width="100" height="100" className ="rulesImage"/>
+        <img src="https://vignette.wikia.nocookie.net/mario/images/8/8c/Mushroom.png/revision/latest?cb=20101223220717"  width="100" height="100" className ="rulesImage"/>
+        
+        <div className = "rulecontainer">
+        
         <h2 className = "rulestitle">Red Mushroom 1 Point</h2>
          </div>
          <div className = "rulecontainer">
         <img src="https://www.mariowiki.com/images/thumb/5/57/Powerup-mini-mushroom-sm.png/200px-Powerup-mini-mushroom-sm.png"  width="100" height="100" className ="rulesImage"/>
+        <img src="https://www.mariowiki.com/images/thumb/5/57/Powerup-mini-mushroom-sm.png/200px-Powerup-mini-mushroom-sm.png"  width="100" height="100" className ="rulesImage"/>
+
         <h2 className = "rulestitle">Blue Mushroom 2 Points</h2>
          </div>
          <div className = "rulecontainer">
@@ -24,10 +30,11 @@ export default class Rules extends PureComponent {
          </div>
          <div className = "rulecontainer">
         <img src="https://vignette.wikia.nocookie.net/nintendo/images/1/12/1upshroom.png/revision/latest?cb=20160922033647&path-prefix=en"  width="100" height="100" className ="rulesImage"/>
+        <img src="https://vignette.wikia.nocookie.net/nintendo/images/1/12/1upshroom.png/revision/latest?cb=20160922033647&path-prefix=en"  width="100" height="100" className ="rulesImage"/>
         <h2 className = "rulestitle">Poisoned Mushroom -1 Point</h2>
          </div>
          <div className = "rulecontainer">
-        <img src="http://www.stickpng.com/assets/thumbs/584df3956a5ae41a83ddee07.png"  width="100" height="100" className ="rulesImage"/>
+        <img src="http://www.stickpng.com/assets/thumbs/584df3956a5ae41a83ddee07.png"  width="200" height="200" className ="rulesImage"/>
         <h2 className = "rulestitle">The player who collects 10 points first wins!</h2>
          </div>
             <br/>
